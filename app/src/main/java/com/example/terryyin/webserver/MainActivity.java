89eb6f7b73b95c4fdd2179d25556e68cb6376c75
terry.yin <haluto8086@sina.cn>
2018-05-05 22:15:19 +0800

    add nanohttpd to support web server.

--- app/src/main/java/com/example/terryyin/webserver/MainActivity.java
+++ app/src/main/java/com/example/terryyin/webserver/MainActivity.java
@@ -3,11 +3,77 @@ package com.example.terryyin.webserver;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import android.os.Environment;
+import android.util.Log;
+import java.io.*;
+import java.util.*;
+
 public class MainActivity extends AppCompatActivity {
 
+    /*
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    */
+    private WebServer server;
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        server = new WebServer();
+        try {
+            server.start();
+        } catch(IOException ioe) {
+            Log.w("Httpd", "The server could not start.");
+        }
+        Log.w("Httpd", "Web server initialized.");
+    }
+
+    // DON'T FORGET to stop the server
+    @Override
+    public void onDestroy()
+    {
+        super.onDestroy();
+        if (server != null)
+            server.stop();
+    }
+
+
+    private class WebServer extends NanoHTTPD {
+
+        public WebServer()
+        {
+            super(8080);
+        }
+
+        @Override
+        public Response serve(String uri, Method method,
+                              Map<String, String> header,
+                              Map<String, String> parameters,
+                              Map<String, String> files) {
+            String answer = "";
+            try {
+                // Open file from SD Card
+                File root = Environment.getExternalStorageDirectory();
+                FileReader index = new FileReader(root.getAbsolutePath() +
+                        "/www/index.html");
+                BufferedReader reader = new BufferedReader(index);
+                String line = "";
+                while ((line = reader.readLine()) != null) {
+                    answer += line;
+                }
+                reader.close();
+
+            } catch(IOException ioe) {
+                Log.w("Httpd", ioe.toString());
+            }
+
+
+            return new NanoHTTPD.Response(answer);
+        }
     }
 }
